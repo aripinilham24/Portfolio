7c0385bbd86edc2b848a40432e9a472191a71cab
aripinilham24 <muhamadarifinilham24@gmail.com>
2026-03-18 02:52:23 +0700

    fix: fix error type

--- component/CertificatesBox.tsx
+++ component/CertificatesBox.tsx
@@ -1,34 +1,41 @@
 import { motion } from "motion/react";
 
-const CertificatesBox = ({ img, title, desc, link }) => {
-    return (
-        <motion.a
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{
-                duration: 0.8,
-                delay: 0.5,
-                ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className="card p-5 bg-gray-950/50 rounded text-white shadow-xl relative hover:shadow-xl hover:shadow-blue-300 transition-shadow"
-            href={link}
-            target="blank"
-        >
-            <img
-                src={img}
-                className="w-full object-cover rounded shadow-x mb-3"
-                alt={title}
-            />
-            <div className="card-body">
-                <h3 className="text-xl md:text-2xl font-bold mb-3">{title}</h3>
-                <p>{desc}</p>
-                {/* <a href={link} className="btn btn-primary">
+interface props {
+  img: string;
+  title: string;
+  desc: string;
+  link: string;
+}
+
+const CertificatesBox = ({ img, title, desc, link }: props) => {
+  return (
+    <motion.a
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+      className="card p-5 bg-gray-950/50 rounded text-white shadow-xl relative hover:shadow-xl hover:shadow-blue-300 transition-shadow"
+      href={link}
+      target="blank"
+    >
+      <img
+        src={img}
+        className="w-full object-cover rounded shadow-x mb-3"
+        alt={title}
+      />
+      <div className="card-body">
+        <h3 className="text-xl md:text-2xl font-bold mb-3">{title}</h3>
+        <p>{desc}</p>
+        {/* <a href={link} className="btn btn-primary">
                     Check
                 </a> */}
-            </div>
-        </motion.a>
-    );
+      </div>
+    </motion.a>
+  );
 };
 
 export default CertificatesBox;
